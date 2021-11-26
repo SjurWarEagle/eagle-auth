b1539f149dbd748893089b3dc0d14c73a5d532e7
Torsten Kunkel <git@tkunkel.de>
2021-11-26 05:24:05 +0100

    * strip auth-header

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,17 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './controller/app.controller';
-import { AppService } from './services/app.service';
+import {Module} from '@nestjs/common';
+import {AppController} from './controller/app.controller';
+import {AppService} from './services/app.service';
+import {HeaderDataService} from "./services/header-data.service";
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+    imports: [],
+    controllers: [
+        AppController
+    ],
+    providers: [
+        AppService,
+        HeaderDataService,
+    ],
 })
-export class AppModule {}
+export class AppModule {
+}
